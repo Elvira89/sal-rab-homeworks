5944366f26ae0fae413d47012187db8fbab0f2a7
Elvira89 <avzalova.e@brinex.ru>
2022-02-26 18:21:35 +0300

    task_1 all

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,10 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
+    newValue = newValue + 1;
+
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -27,12 +31,15 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    // замените склеивание, используемое в newSum Text на шаблонную строку/
 
     // Конец решения задания №1.3.
 
