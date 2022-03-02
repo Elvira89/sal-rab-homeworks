99f72119943410ac9dd776dc49b7e2ab95f90d47
Elvira89 <avzalova.e@brinex.ru>
2022-03-02 13:22:54 +0300

    task_4 all

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,12 +2,24 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
+let data;
+data =JSON.parse(json);
+let products;
+products = data.products;
+return products;
+}
+function renderProductsCards(json){
+    clearProducts();
+    products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);            
+    }
+}
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
